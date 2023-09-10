fae5ecc1e68ffaff1f4e6d0b0d56e9a9ee6d2905
Juan Carlos Tinoco Licas <jtinocol@osinergmin.gob.pe>
2023-09-10 11:55:15 -0500

    feat: implemented redirection to default page home

--- src/router/index.js
+++ src/router/index.js
@@ -5,9 +5,13 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/home',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/',
+      redirect: 'home'
     },
     {
       path: '/about',
