816cb32b43c92528ce1efee752e0ca53d7914f42
Juan Carlos Tinoco Licas <jtinocol@osinergmin.gob.pe>
2023-09-10 14:46:56 -0500

    chore: update about and home components

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeComponent from "../public/pages/home.component.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +7,7 @@ const router = createRouter({
     {
       path: '/home',
       name: 'home',
-      component: HomeView
+      component: HomeComponent
     },
     {
       path: '/',
@@ -19,7 +19,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../public/pages/about.component.vue')
     },
     {
       path: "/tutorials",
